d2936ef5014d0862cbe70e82a2be12bb9f1ebef2
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-09 19:52:52 +0530

    Automated commit: frontend/src/components/Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2023 Tic-Tac-Toe Game</p>
+    </footer>
+  );
+}
+
+export default Footer;
